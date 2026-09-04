1164b0540f173c79b127f27c8fdbdd32851dfbe8
David Minaev <globalarray@mail.ru>
2026-09-05 01:42:14 +0300

    refactor: remove handler UUID helper

--- backend/internal/handler/project.go
+++ backend/internal/handler/project.go
@@ -1,23 +1,11 @@
 package handler
 
 import (
-	"crypto/rand"
 	"errors"
-	"fmt"
 	"time"
 
 	"github.com/Benzocloud/cosmohack/backend/internal/service/store"
 )
-
-func newUUID() (string, error) {
-	var b [16]byte
-	if _, err := rand.Read(b[:]); err != nil {
-		return "", err
-	}
-	b[6] = (b[6] & 0x0f) | 0x40
-	b[8] = (b[8] & 0x3f) | 0x80
-	return fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:]), nil
-}
 
 func formatTime(t time.Time) string {
 	return t.UTC().Truncate(time.Second).Format("2006-01-02T15:04:05Z")
